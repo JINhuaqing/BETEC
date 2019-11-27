3a64cb4f96f8841ca57350744af9902374d0d8c1
JINhuaqing <kevinjin0423@gmail.com>
2019-11-27 21:33:46 +0800

    update plot script

--- densityplot.R
+++ densityplot.R
@@ -4,37 +4,42 @@ post.f <- function(r, n){
   ff
 }
 
-# Both method
-f1 <- post.f(28, 91)
-f2 <- post.f(26, 70)
-f3 <- post.f(23, 46)
-f4 <- post.f(31, 52)
-curve(f1, 0, 1, lty=1, xlab="p", ylab="Density", xaxt="n")
-curve(f2, 0, 1, lty=2, add=TRUE)
-curve(f3, 0, 1, lty=3, add=TRUE)
-curve(f4, 0, 1, lty=4, add=TRUE)
+# bBET
+f1 <- post.f(26, 84)
+f2 <- post.f(25, 67)
+f3 <- post.f(20, 40)
+f4 <- post.f(16, 25)
+jpeg(file="bBET.jpg")
+curve(f1, 0, 1, lty=1, lwd=2, xlab="p", ylab="Density", xaxt="n")
+curve(f2, 0, 1, lty=2, lwd=2, add=TRUE)
+curve(f3, 0, 1, lty=3, lwd=2, add=TRUE)
+curve(f4, 0, 1, lty=4, lwd=2, add=TRUE)
 
 ex1 <- expression(paste(p[0]==0.05, ", ", p[1]==0.25))
 ex2 <- expression(paste(p[0]==0.1, ", ", p[1]==0.3))
 ex3 <- expression(paste(p[0]==0.2, ", ", p[1]==0.4))
 ex4 <- expression(paste(p[0]==0.3, ", ", p[1]==0.5))
 axis(1, seq(0, 1, 0.05), seq(0, 1, 0.05))
-legend(0.6, 8.5, c(ex1, ex2, ex3, ex4), lty=1:4)
+legend(0.6, 8, c(ex1, ex2, ex3, ex4), lty=1:4)
+dev.off()
 
 
-# delta method
-f1 <- post.f(16, 48)
-f2 <- post.f(20, 52)
-f3 <- post.f(26, 53)
-f4 <- post.f(31, 52)
-curve(f1, 0, 1, lty=1, xlab="p", ylab="Density", xaxt="n")
-curve(f2, 0, 1, lty=2, add=TRUE)
-curve(f3, 0, 1, lty=3, add=TRUE)
-curve(f4, 0, 1, lty=4, add=TRUE)
+# delta-bBET
+f1 <- post.f(14, 41)
+f2 <- post.f(16, 41)
+f3 <- post.f(22, 44)
+f4 <- post.f(26, 43)
+
+jpeg(file="delta-bBET.jpg")
+curve(f1, 0, 1, lty=1, lwd=2, xlab="p", ylab="Density", xaxt="n")
+curve(f2, 0, 1, lty=2, lwd=2, add=TRUE)
+curve(f3, 0, 1, lty=3, lwd=2, add=TRUE)
+curve(f4, 0, 1, lty=4, lwd=2, add=TRUE)
 
 ex1 <- expression(paste(p[0]==0.05, ", ", p[1]==0.25))
 ex2 <- expression(paste(p[0]==0.1, ", ", p[1]==0.3))
 ex3 <- expression(paste(p[0]==0.2, ", ", p[1]==0.4))
 ex4 <- expression(paste(p[0]==0.3, ", ", p[1]==0.5))
 axis(1, seq(0, 1, 0.05), seq(0, 1, 0.05))
-legend(0.6, 6, c(ex1, ex2, ex3, ex4), lty=1:4)
+legend(0.6, 5.5, c(ex1, ex2, ex3, ex4), lty=1:4)
+dev.off()
